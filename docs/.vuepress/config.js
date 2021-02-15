b15b7207e2cae33d6279784b75a99ef4ca6d45fc
Ginakira <ginakira@foxmail.com>
2021-02-15 19:25:14 +0800

    Initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: 'PMR字幕组烤肉指北',
+    description: '全新2.0版本 只为高效烤肉',
+    themeConfig: {
+        logo: '/images/logo.png',
+        nav: [
+            { text: 'Acfun', link: 'https://www.acfun.cn/u/14473357' },
+            { text: 'Bilibili', link: 'https://space.bilibili.com/7151607' }
+        ],
+        lastUpdated: '上次更新',
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroText: PMR字幕组烤肉指北
+heroImage: /images/logo.png
+tagline: 船新2.0版本 只为高效烤肉
+actionText: 砍柴生火 →
+actionLink: /zh/guide/
+features:
+  - title: 为爱发电
+    details: 因为热爱，所以选择。
+  - title: 用心译制
+    details: 竭尽所能，为观众献上高质量的中文字幕。
+  - title: 愉快烤肉
+    details: 没有硬性指派，没有绩效考核，一切全凭你的内心。
+footer: 采用 MIT 开源协议 | Copyright © 2018-2021 Ginakira
+---
