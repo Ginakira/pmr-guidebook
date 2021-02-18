97caf7383d559f287e09fa7f31391cea5d74b623
Ginakira <ginakira@foxmail.com>
2021-02-18 19:51:05 +0800

    关于页面

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,6 +5,7 @@ module.exports = {
         logo: '/images/logo.png',
         nav: [
             { text: '指北', link: '/guide/' },
+            { text: '关于我们', link: '/about/'},
             { text: 'Acfun', link: 'https://www.acfun.cn/u/14473357' },
             { text: 'Bilibili', link: 'https://space.bilibili.com/7151607' },
             { text: 'Github', link: 'https://github.com/Ginakira/pmr-guidebook' }
@@ -15,7 +16,8 @@ module.exports = {
         sidebar: {
             '/guide/': [
                 '',
-                'workflow'
+                'workflow',
+                'translate'
             ]
         }
     }
